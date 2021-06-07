a2360464e2309dae38406592c2cdf7b4eb7d2b47
kokasantosh <59968691+kokasantosh@users.noreply.github.com>
2021-06-07 08:15:37 +0530

    Added ArrayToBalancedTreeConverter

---
+++ src/com/sample/tree/ArrayToBalancedTreeConverter.java
@@ -0,0 +1,21 @@
+package com.sample.tree;
+
+public class ArrayToBalancedTreeConverter {
+    public static void main(String[] args) {
+        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7};
+        TreeNode root = convertToBalacedTree(arr, 0, arr.length -1);
+        root.printBFS();
+    }
+
+    private static TreeNode convertToBalacedTree(int[] arr, int start , int end) {
+        if(arr.length == 0 || start>end) {
+            return null;
+        }
+        int middleIndex = (start+end)/2;
+        TreeNode node = new TreeNode(arr[middleIndex]);
+        node.setLeft(convertToBalacedTree(arr, start, middleIndex -1));
+        node.setRight(convertToBalacedTree(arr, middleIndex + 1, end));
+        return node;
+    }
+
+}
